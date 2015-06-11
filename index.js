f86b3ad5d91a0aefcd5ef9b212b7df83ffd873b5
Sumved Shami <sumved.shami@gmail.com>
2015-06-11 15:07:46 +0530

    Added GET api.

--- index.js
+++ index.js
@@ -15,54 +15,53 @@ server.connection({
 
 // Add the route
 server.route({
-    method: 'POST',
+    method: 'GET',
     path: '/places',
-    handler: function(request, reply) {
-    	
-        var payload = request.payload;
-        console.log('Request payload:\n' + JSON.stringify(payload));
-
-        parameters = {
-            location: [payload.location.latitude, payload.location.longitude],
-            types: payload.keyword
-        };
-
-        googlePlaces.placeSearch(parameters, function(error, places_response) {
-
-            if (error) {
-                throw error;
-            }
-
-            var places = places_response.results;
-            console.log(JSON.stringify(places, null, 2));
-
-            var arr = new Array();
-
-            async.each(places, function(place, callback) {
-
-                var location = {
-                    latitude: place.geometry.location.lat,
-                    longitude: place.geometry.location.lng
-                };
-
-                var splits = place.vicinity.split(', ');
-                var city = splits[splits.length - 1];
-
-                var data = {
-                    name: place.name,
-                    icon: place.icon,
-                    location: location,
-                    address: {
-                        city: city
-                    }
-                };
-                arr.push(data);
-            });
-
-            reply(arr);
-        });
-    }
+    handler: getGooglePlaces
 });
+
+function getGooglePlaces(request, reply) {
+
+    parameters = {
+    	location: [request.query.latitude, request.query.longitude],
+    	types: request.query.keyword
+    };
+
+    googlePlaces.placeSearch(parameters, function(error, places_response) {
+
+        if (error) {
+            throw error;
+        }
+
+        var places = places_response.results;
+        console.log(JSON.stringify(places, null, 2));
+
+        var arr = new Array();
+
+        async.each(places, function(place, callback) {
+
+            var location = {
+                latitude: place.geometry.location.lat,
+                longitude: place.geometry.location.lng
+            };
+
+            var splits = place.vicinity.split(', ');
+            var city = splits[splits.length - 1];
+
+            var data = {
+                name: place.name,
+                icon: place.icon,
+                location: location,
+                address: {
+                    city: city
+                }
+            };
+            arr.push(data);
+        });
+
+        reply(arr);
+    });
+}
 
 // Start the server
 server.start();
